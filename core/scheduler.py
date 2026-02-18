6d9ba1467f243b9f8ef709350172400850edebff
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2026-02-19 02:33:21 +0530

    .

--- core/scheduler.py
+++ core/scheduler.py
@@ -1,8 +1,18 @@
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from database.reset_manager import reset_today, reset_week
-import datetime
+from database import redis_client
 
 scheduler = AsyncIOScheduler()
+
+
+def reset_today():
+    for key in redis_client.scan_iter("*:today"):
+        redis_client.delete(key)
+
+
+def reset_week():
+    for key in redis_client.scan_iter("*:week"):
+        redis_client.delete(key)
+
 
 def start_scheduler():
     scheduler.add_job(reset_today, "cron", hour=0, minute=0)
