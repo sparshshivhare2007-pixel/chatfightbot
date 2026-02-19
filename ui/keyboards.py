b48deeae0cff6f793002e99f2f482de6e4c5b39f
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2026-02-19 15:18:47 +0530

    Update keyboards.py

--- ui/keyboards.py
+++ ui/keyboards.py
@@ -25,17 +25,40 @@ def ranking_keyboard(mode: str, scope: str, target_id: int):
 
 
 # =========================
-# GLOBAL LEADERBOARD BUTTONS
+# GLOBAL TOP USERS BUTTONS
 # =========================
 
-def global_ranking_keyboard(mode: str):
+def topusers_keyboard(mode: str):
 
     def button(text: str, key: str):
         active = key == mode
 
         return InlineKeyboardButton(
             text=f"{'🟢' if active else '⚪'} {text}",
-            callback_data=f"globalrank:{key}"
+            callback_data=f"topusers:{key}"
+        )
+
+    return InlineKeyboardMarkup([
+        [
+            button("Overall", "overall"),
+            button("Today", "today"),
+            button("Week", "week")
+        ]
+    ])
+
+
+# =========================
+# GLOBAL TOP GROUPS BUTTONS
+# =========================
+
+def topgroups_keyboard(mode: str):
+
+    def button(text: str, key: str):
+        active = key == mode
+
+        return InlineKeyboardButton(
+            text=f"{'🟢' if active else '⚪'} {text}",
+            callback_data=f"topgroups:{key}"
         )
 
     return InlineKeyboardMarkup([
