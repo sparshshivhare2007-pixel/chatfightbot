58cbe7f895181d48199ad1991fb67d69a9020088
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2026-02-19 12:10:15 +0530

    Update message_listener.py

--- core/message_listener.py
+++ core/message_listener.py
@@ -6,26 +6,16 @@ from database import increment_message
 
 async def count_messages(client, message):
 
-    # Only count group messages
     if message.chat.type not in [ChatType.GROUP, ChatType.SUPERGROUP]:
         return
 
-    # Ignore service messages
-    if not message.from_user:
+    if not message.from_user or message.from_user.is_bot:
         return
 
-    # Ignore bots
-    if message.from_user.is_bot:
-        return
-
-    group_id = message.chat.id
-    user_id = message.from_user.id
-
-    # Increment counter
-    await increment_counter(group_id, user_id)
+    # Mongo counter use karo
+    increment_message(message.from_user, message.chat)
 
 
-# Count ALL normal messages (not only text)
 message_counter = MessageHandler(
     count_messages,
     filters.group & ~filters.service
