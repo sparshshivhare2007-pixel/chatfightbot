e9550ec7b035c3b9ec07ba38834f0a02bff81169
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2026-02-19 12:03:33 +0530

    Update message_listener.py

--- core/message_listener.py
+++ core/message_listener.py
@@ -1,12 +1,32 @@
 from pyrogram import filters
 from pyrogram.handlers import MessageHandler
+from pyrogram.enums import ChatType
 from services.counter_service import increment_counter
 
-async def count_messages(client, message):
-    if message.chat.type in ["group", "supergroup"] and not message.from_user.is_bot:
-        await increment_counter(message.chat.id, message.from_user.id)
 
+async def count_messages(client, message):
+
+    # Only count group messages
+    if message.chat.type not in [ChatType.GROUP, ChatType.SUPERGROUP]:
+        return
+
+    # Ignore service messages
+    if not message.from_user:
+        return
+
+    # Ignore bots
+    if message.from_user.is_bot:
+        return
+
+    group_id = message.chat.id
+    user_id = message.from_user.id
+
+    # Increment counter
+    await increment_counter(group_id, user_id)
+
+
+# Count ALL normal messages (not only text)
 message_counter = MessageHandler(
     count_messages,
-    filters.group & filters.text
+    filters.group & ~filters.service
 )
